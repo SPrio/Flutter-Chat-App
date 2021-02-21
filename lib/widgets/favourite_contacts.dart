a7d248c78dc7b3091c1c8eea5a4645aa4b7f321b
SPrio <mahantasupriyo@gmail.com>
2021-02-22 02:04:53 +0530

    added navigation to chatScreen

--- lib/widgets/favourite_contacts.dart
+++ lib/widgets/favourite_contacts.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/models/message_model.dart';
+import 'package:flutter_chat_app/screens/chat_screen.dart';
 
 class FavouriteContacts extends StatelessWidget {
   @override
@@ -33,22 +34,29 @@ class FavouriteContacts extends StatelessWidget {
                 scrollDirection: Axis.horizontal,
                 itemCount: favorites.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 35.0,
-                          backgroundImage:
-                              AssetImage(favorites[index].imageUrl),
-                        ),
-                        SizedBox(height: 6.0),
-                        Text(favorites[index].name,
-                            style: TextStyle(
-                                color: Colors.blueGrey,
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.w600)),
-                      ],
+                  return GestureDetector(
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => ChatScreen(user: favorites[index]),
+                        )),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            radius: 35.0,
+                            backgroundImage:
+                                AssetImage(favorites[index].imageUrl),
+                          ),
+                          SizedBox(height: 6.0),
+                          Text(favorites[index].name,
+                              style: TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w600)),
+                        ],
+                      ),
                     ),
                   );
                 },
