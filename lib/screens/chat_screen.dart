fb2fbc07630381d55df6d7be00f72b2a9c6b4b8b
SPrio <mahantasupriyo@gmail.com>
2021-02-22 02:35:03 +0530

    added chat screen appbar

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/models/message_model.dart';
 import 'package:flutter_chat_app/models/user_model.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -13,6 +14,48 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        appBar: AppBar(
+          title: Text(widget.user.name,
+              style: TextStyle(
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              )),
+          elevation: 0.0,
+          actions: [
+            IconButton(
+                icon: Icon(Icons.more_horiz),
+                iconSize: 30.0,
+                color: Colors.white,
+                onPressed: () {})
+          ],
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                  margin: EdgeInsets.only(top: 20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(30.0),
+                        topLeft: Radius.circular(30.0)),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(30.0),
+                        topLeft: Radius.circular(30.0)),
+                    child: ListView.builder(
+                        padding: EdgeInsets.only(top: 15.0),
+                        itemCount: messages.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final Message message = messages[index];
+                          return Text(message.text);
+                        }),
+                  )),
+            ),
+          ],
+        ));
   }
 }
