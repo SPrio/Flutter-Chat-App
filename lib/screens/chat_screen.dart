7c2d1bdcd00076f57b8930c63cb34e99db1c158c
SPrio <mahantasupriyo@gmail.com>
2021-02-24 02:33:10 +0530

    added text field in chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -62,6 +62,32 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 
+  _buildMessageComposer() {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        height: 70.0,
+        color: Colors.white,
+        child: Row(children: [
+          IconButton(
+              icon: Icon(Icons.photo),
+              iconSize: 25.0,
+              color: Theme.of(context).primaryColor,
+              onPressed: () {}),
+          Expanded(
+              child: TextField(
+            textCapitalization: TextCapitalization.sentences,
+            onChanged: (value) {},
+            decoration:
+                InputDecoration.collapsed(hintText: 'Send a message...'),
+          )),
+          IconButton(
+              icon: Icon(Icons.send),
+              iconSize: 25.0,
+              color: Theme.of(context).primaryColor,
+              onPressed: () {})
+        ]));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -81,33 +107,38 @@ class _ChatScreenState extends State<ChatScreen> {
                 onPressed: () {})
           ],
         ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Container(
-                  margin: EdgeInsets.only(top: 20.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(30.0),
-                        topLeft: Radius.circular(30.0)),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(30.0),
-                        topLeft: Radius.circular(30.0)),
-                    child: ListView.builder(
-                        reverse: true,
-                        padding: EdgeInsets.only(top: 15.0),
-                        itemCount: messages.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          final Message message = messages[index];
-                          final bool isMe = message.sender.id == currentUser.id;
-                          return _buildMessage(message, isMe);
-                        }),
-                  )),
-            ),
-          ],
+        body: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                    margin: EdgeInsets.only(top: 20.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(30.0),
+                          topLeft: Radius.circular(30.0)),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(30.0),
+                          topLeft: Radius.circular(30.0)),
+                      child: ListView.builder(
+                          reverse: true,
+                          padding: EdgeInsets.only(top: 15.0),
+                          itemCount: messages.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final Message message = messages[index];
+                            final bool isMe =
+                                message.sender.id == currentUser.id;
+                            return _buildMessage(message, isMe);
+                          }),
+                    )),
+              ),
+              _buildMessageComposer()
+            ],
+          ),
         ));
   }
 }
